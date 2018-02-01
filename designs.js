1600e46dd6471af074e12e553cb3440dffa5e0ef
galina <galina@energysavvy.com>
2018-01-31 20:51:37 -0800

    include jquery, parse input

--- designs.js
+++ designs.js
@@ -4,7 +4,24 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    var height = $('#inputHeight').val();
+    var width = $('#inputWidth').val();
+    var grid = ''
+        for (row = 0; row > parseInt(height); row++){
+            grid += '<tr>';
+            for (column = 0; parseInt(column) > 0; column++){
+                grid +=  '<td></td>';
+            }
+            grid += '</tr>';
+        };
+    console.log(grid);
+    $('#pixelCanvas').append(grid);
 
-// Your code goes here!
+};
 
-}
+$( document ).ready(function() {
+
+    $('#sizePicker').submit(makeGrid());
+
+});
+
