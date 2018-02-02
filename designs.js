8444fa10e9c03855a22015ff94a7a62f951d8e37
galina <galina@energysavvy.com>
2018-02-01 19:55:43 -0800

    parse int at the time of setting a variable

--- designs.js
+++ designs.js
@@ -4,13 +4,13 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-    var height = $('#inputHeight').val();
-    var width = $('#inputWeight').val();
+    var height = parseInt($('#inputHeight').val());
+    var width = parseInt($('#inputWeight').val());
     var grid = '';
 
-    for (row = 0; row < parseInt(height); row++){
+    for (row = 0; row < height; row++){
         grid += '<tr>';
-        for (column = 0; column < parseInt(width); column++){
+        for (column = 0; column < width; column++){
             grid +=  '<td></td>';
         }
         grid += '</tr>';
